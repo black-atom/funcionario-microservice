ef6aa567c09233b9d39cefb29458ac521f79048c
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-04-25 15:02:30 -0300

    added user controller

---
+++ controllers/usersController.js
@@ -0,0 +1,23 @@
+const user = require ('./../models/user');
+
+exports.getAllUsers = (req, res, next) => {
+    user.find().then(foundUsers => {
+        
+        res.status(200)
+        .json(foundUsers);
+
+    }).catch(error => {
+        next(error);
+    })
+}
+
+exports.getOneUser = (req, res, next) => {
+    user.findById(req.params.id).then(foundOneUser => {
+
+        res.status(200)
+        .json(foundOneUser);
+
+    }).catch(error => {
+        next(error);
+    })
+}
