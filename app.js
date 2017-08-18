bdff6232cf6ed1b7099f8ecf4823612dcced77ab
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-08-18 12:39:29 -0300

    removing unused stuff

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 if (app.get('env') === 'development') {
 	app.use((err, req, res, next) => {
 		res.status(err.status || 500);
-		res.render('error', {
+		res.send('error', {
 			message: err.message,
 			error: err
 		});
@@ -58,7 +58,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
 	res.status(err.status || 500);
-	res.render('error', {
+	res.send('error', {
 		message: err.message,
 		error: {}
 	});
