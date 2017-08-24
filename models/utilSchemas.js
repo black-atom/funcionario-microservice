ebd960d55c1e4b2c50d2b926a0a18db3505a6d6a
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-08-24 13:03:40 -0300

    changing estado to uf

--- models/utilSchemas.js
+++ models/utilSchemas.js
@@ -4,7 +4,7 @@ const enderecoSchema  = new mongoose.Schema({
     complemento: { type: String, default: "" },
     bairro: {type: String, required: [true, "Entre com o bairro"]},
     cidade: {type: String, required: [true, "Entre com a cidade"]},
-    estado: {type: String, required: [true, "Entre com os dados do estado"]},
+    uf: {type: String, required: [true, "Entre com os dados do estado"]},
     ponto_referencia: { type: String, default: "" },
     cep: {type: String, required: [true, "Entre com o cep"]}
 })
