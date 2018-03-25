f996347c03f8016ddd87b58cfb39b32236c80c4f
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2018-03-25 14:39:41 -0300

    config: change the authconfig settings

--- config/authConfig.js
+++ config/authConfig.js
@@ -3,17 +3,23 @@ const getConfig = require('./index');
 const config = getConfig({
     development: {
         bypass: true,
-        secret: "realponto"
+        secret: "realponto",
+        host: 'localhost',
+        port: 3000
     },
 
     test: {
         bypass: true,
-        secret: "realponto"
+        secret: "realponto",
+        host: 'localhost',
+        port: 3000
     },
 
     production: {
         bypass: false,
-        secret: "realponto"
+        secret: "realponto",
+        host: process.env.AUTH_API_HOST || 'localhost',
+        port: process.env.AUTH_API_PORT ||  3000
     }
 });
 
