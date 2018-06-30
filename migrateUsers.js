4d404087ea3f8f0aba3f467127c2c4951be7b982
vitorsilvalima <vitorsilvalima29@gmail.com>
2018-06-30 11:47:30 -0300

    script: migrate script to update api-keys

--- migrateUsers.js
+++ migrateUsers.js
@@ -1,9 +1,16 @@
 const Funcionarios = require('./models/funcionario');
-const uuidv4 = require('uuid/v4')
+const uuidv4 = require('uuid/v4');
+const Promise = require('bluebird');
  
 const updateFuncionarios = async() => {
   try {
-    const funcionarios = await Funcionarios.updateMany({}, { $set: { 'login.api_key': uuidv4() }});
+    const funcionarios = await Funcionarios.find({})
+    await Promise.resolve(funcionarios)
+      .map(funcionario => {
+        funcionario.login.api_key = uuidv4()
+        return funcionario.save()
+      })
+
     console.log('ok!')
   } catch (err) {
     console.log(err);
